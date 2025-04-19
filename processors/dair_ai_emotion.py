dcac5d86fb46b3c012c0e5643045ee27ea07dc1f
= <=>
2025-04-19 17:30:15 +0300

    added processors

---
+++ processors/dair_ai_emotion.py
@@ -0,0 +1,57 @@
+from typing import List
+from transformers import TrainerCallback
+import evaluate
+
+from callbacks import ClassEvalCallback
+from .processor import BaseProcessor
+
+
+class EmotionProcessor(BaseProcessor):
+    def __init__(
+        self, 
+        *, 
+        metrics: List[str] = ['accuracy'],
+        eval_steps: int = 8000,
+        eval_batch_size: int = 10,
+        **kwargs
+    ):
+        """
+        Args:
+            metrics (List[str]): List of metrics to evaluate on. Default is ['accuracy'].
+            eval_steps (int): The number of steps between evaluations. Default is 8000.
+            eval_batch_size (int): Batch size for evaluation. Default is 10.
+            **kwargs: Additional arguments passed to the BaseProcessor.
+        """
+        self._labels_list = ["sadness","joy","love","anger","fear","surprise"]
+        
+        super().__init__(**kwargs)
+        
+        self._metrics = metrics
+        self._eval_steps = eval_steps
+        self._eval_batch_size = eval_batch_size
+        
+
+
+    def process_row(self, row):
+        prompt = f"Below is a piece of text. Classify it into one of: {', '.join(self._labels_list)}.\n\n"
+        prompt += f"\"{row['text']}\"\n\nThe emotion in the above text is: "
+        response = f"{self._labels_list[row['label']]}"
+        return {
+            "prompt": prompt,
+            "response": response,
+        }
+        
+
+    def get_callbacks(self) -> List[TrainerCallback]:
+        return [
+            ClassEvalCallback(
+                eval_ds=self.tokenized_validation,
+                tokenizer=self._tokenizer,
+                model=self._model,
+                metrics=[evaluate.load(metric) for metric in self._metrics],
+                eval_steps=self._eval_steps,
+                batch_size=self._eval_batch_size,
+                labels_list=self._labels_list,
+                max_out_tokens=self._max_output_tokens
+            )
+        ]
---
+++ processors/processor.py
@@ -0,0 +1,160 @@
+from typing import Dict, Literal, Any, List, Union, Optional
+from abc import ABC, abstractmethod
+from transformers import TrainerCallback, PreTrainedTokenizer, PreTrainedModel
+from datasets import DatasetDict, Dataset, IterableDatasetDict, IterableDataset
+
+
+class BaseProcessor(ABC):
+    """
+    Base class for all processors. This class is responsible for processing the dataset and preparing it for training.
+    It handles prompt creation, tokenization, padding, and callbacks during the training.
+    """
+    def __init__(
+        self,
+        *,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        dataset: Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset],
+        max_input_tokens: Optional[int],
+        max_output_tokens: Optional[int],
+    ):
+        """
+        Args:
+            model (PreTrainedModel): The model to be trained.
+            tokenizer (PreTrainedTokenizer): The tokenizer used for the model.
+            dataset (Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]): The dataset to be used - should have a train/validation split.
+            max_input_tokens (Optional[int]): The maximum number of input tokens.
+            max_output_tokens (Optional[int]): The maximum number of output tokens.
+        """
+        self._model = model
+        self._tokenizer = tokenizer
+        self._dataset = dataset
+        self._max_input_tokens = max_input_tokens
+        self._max_output_tokens = max_output_tokens
+        self.__autoset_input_output_tokens()
+        
+        self._processed_train = None
+        self._tokenized_validation = None
+        
+        
+    @property
+    def processed_train(self):
+        """
+        Returns the processed training dataset.
+        """
+        if self._processed_train:
+            return self._processed_train
+
+        self._processed_train = self._dataset["train"].map(
+            self._preprocess_train, batched=True,
+            remove_columns=self._dataset["train"].column_names
+        )
+        return self._processed_train
+
+
+    @property
+    def tokenized_validation(self):
+        """
+        Returns the processed validation dataset.
+        """
+        if self._tokenized_validation:
+            return self._tokenized_validation
+        self._tokenized_validation = self._dataset["validation"].map(
+            self._preprocess_eval, batched=True,
+            remove_columns=self._dataset["validation"].column_names
+        )
+        return self._tokenized_validation
+    
+    
+    def __autoset_input_output_tokens(self) -> None:
+        """
+        Automatically sets the maximum input and output tokens based on the dataset.
+        """
+        max_input = 0
+        max_output = 0
+        
+        for dataset in self._dataset.values():
+            for row in dataset:
+                parsed_row = self.process_row(row)
+                input = self._tokenizer(parsed_row['prompt'], truncation=True, max_length=self._max_input_tokens)
+                output = self._tokenizer(parsed_row['response'], truncation=True, max_length=self._max_output_tokens)
+                max_input = max(max_input, len(input.input_ids))
+                max_output = max(max_output, len(output.input_ids))
+        
+        if self._max_input_tokens is None:
+            self._max_input_tokens = max_input
+            print(f"Auto-set max input tokens to {self._max_input_tokens}.")
+        if self._max_output_tokens is None:
+            self._max_output_tokens = max_output
+            print(f"Auto-set max output tokens to {self._max_output_tokens}.")
+        
+        
+    def _preprocess_train(
+        self, 
+        examples: Dict[str, List[Any]], 
+    ):
+        """
+        Preprocesses the training dataset by tokenizing the input and output sequences.
+        """
+        input_ids, attn_mask, labels = [], [], []
+        rows = [dict(zip(examples, t)) for t in zip(*examples.values())]
+        for row in rows:
+            parsed_row = self.process_row(row)
+            p = self._tokenizer(parsed_row['prompt'], truncation=True, max_length=self._max_input_tokens)
+            r = self._tokenizer(parsed_row['response'], truncation=True, max_length=self._max_output_tokens)
+
+            seq_ids = p.input_ids + r.input_ids
+            mask    = [1] * len(seq_ids)  # Attend to all input and output tokens
+            lab     = [-100] * len(p.input_ids) + r.input_ids  # Loss will not be computed for input tokens
+
+            # Right pad the sequence to the maximum length
+            pad_len = self._max_input_tokens + self._max_output_tokens - len(seq_ids)
+            seq_ids += [self._tokenizer.pad_token_id] * pad_len
+            mask    += [0] * pad_len  # Do not attend to padding tokens
+            lab     += [-100] * pad_len  # Loss will not be computed for padding tokens
+
+            input_ids.append(seq_ids)
+            attn_mask.append(mask)
+            labels.append(lab)
+
+        return {
+            "input_ids":      input_ids,
+            "attention_mask": attn_mask,
+            "labels":         labels,
+        }
+        
+    def _preprocess_eval(
+        self, 
+        examples: List[Dict[str, Any]], 
+    ):
+        """
+        Preprocesses the evaluation dataset by tokenizing the input and output sequences.
+        """
+        rows = [dict(zip(examples, t)) for t in zip(*examples.values())]
+        parsed_rows = [self.process_row(row) for row in rows]
+        prompts = [row['prompt'] for row in parsed_rows]
+        inputs = self._tokenizer(prompts, truncation=True, padding="max_length", max_length=self._max_input_tokens)
+        inputs["labels"] = [row['response'] for row in parsed_rows]
+        return inputs
+    
+    
+    @abstractmethod
+    def process_row(self, row: dict[str, Any]) -> Dict[Literal["prompt", "response"], str]:
+        """
+        Processes a single row of the dataset. This method should be implemented by subclasses.
+        Args:
+            row (dict[str, Any]): A single row of the dataset.
+        Returns:
+            Dict[Literal["prompt", "response"], str]: A dictionary containing the prompt and response for the given row.
+        """
+        pass
+    
+    @abstractmethod
+    def get_callbacks(self) -> List[TrainerCallback]:
+        """
+        Returns a list of callbacks to be used during training. This method should be implemented by subclasses.
+        Returns:
+            List[TrainerCallback]: A list of callbacks to be used during training.
+        """
+        pass
+        
