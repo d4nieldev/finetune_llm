946b9215978c0ddded253d536a79a1a8e0e27596
d4nieldev <d4nieldev@gmail.com>
2025-04-23 16:56:53 +0000

    updated running script

--- runfast.sh
+++ runfast.sh
@@ -1,10 +1,19 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+# Enter virtual environment
+if [ -d ".venv" ]; then
+    source .venv/bin/activate
+else
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+# Tokens
 export HF_TOKEN=""
 export WANDB_API_KEY=""
+
+# Run the script in the background
 pid = $(nohup accelerate launch finetune_sft.py > output.log 2>&1 &)
+
 echo "Finetuning started with PID: $pid"
 echo "To stop the process, use: kill $pid"
-echo "To view the logs, use: tail -f output.log"
-echo "To view the process list, use: ps aux | grep finetune_sft.py"
+echo "To view the logs view output.log"
