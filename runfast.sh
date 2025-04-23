0b895fec50d1b3617d2b0ed0ce4f87abdc25c2ea
d4nieldev <d4nieldev@gmail.com>
2025-04-23 07:40:50 +0000

    added prints and wandb key

--- runfast.sh
+++ runfast.sh
@@ -2,4 +2,9 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 export HF_TOKEN=""
-nohup accelerate launch finetune_sft.py > output.log 2>&1 &
+export WANDB_API_KEY=""
+pid = $(nohup accelerate launch finetune_sft.py > output.log 2>&1 &)
+echo "Finetuning started with PID: $pid"
+echo "To stop the process, use: kill $pid"
+echo "To view the logs, use: tail -f output.log"
+echo "To view the process list, use: ps aux | grep finetune_sft.py"
