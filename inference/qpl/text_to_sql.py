1e3434769997b55dcf95f67138216636692b2990
Daniel Ohayon <danielohayon2019@gmail.com>
2025-05-16 14:05:26 +0300

    partial implementation

--- inference/qpl/text_to_sql.py
+++ inference/qpl/text_to_sql.py
@@ -1,11 +1,26 @@
 from .qpl_to_cte import flat_qpl_to_cte
 from processors.qpl import QPLDecomposerProcessor, QPLComposerProcessor
+from typing_extensions import TypedDict
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
 
 
-def text_to_qpl(example: dict, db_id: str, schema: str) -> str:
+class InputExample(TypedDict):
+    question: str
+    db_id: str
+
+
+def text_to_qpl(example: InputExample) -> str:
     decomposer_processor = QPLDecomposerProcessor()
     composer_processor = QPLComposerProcessor()
 
 
+def decompose_nlq(
+        example: InputExample, 
+        processor: QPLDecomposerProcessor,
+        model: AutoPeftModelForCausalLM,
+        tokenizer: AutoTokenizer,
+    ) -> str:
 
+    chat_template = processor.to_chat_template(example)
 
