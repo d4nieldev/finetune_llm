14df994f77ca6b56da77eaac7b03761fb8c1e390
= <=>
2025-05-16 16:29:42 +0300

    preperation for completion implementation

--- inference/qpl/text_to_sql.py
+++ inference/qpl/text_to_sql.py
@@ -36,32 +36,58 @@ class Operator(Enum):
 @dataclass
 class QPLTree:
     question: str
-    op: Operator
+    op: Operator = None
+    qpl_line: str = None
     children: Optional[Union[Tuple["QPLTree"], Tuple["QPLTree", "QPLTree"]]] = None
 
     def to_dict(self) -> Dict[str, Any]:
         return {
             "question": self.question,
             "op": self.op.value,
+            "qpl_line": self.qpl_line,
             "children": [child.to_dict() for child in self.children] if self.children else None
         }
 
+    @property
+    def prefix_qpl(self) -> str:
+        if self.children is None:
+            return ""
+        return "\n".join([(child.prefix_qpl + "\n" + child.qpl_line).strip() for child in self.children]).replace("\n", " ; ")
+    
+    @property
+    def qpl(self) -> str:
+        return f"{self.prefix_qpl}\n{self.qpl_line}" if self.prefix_qpl else self.qpl_line
 
-def text_to_qpl(examples: List[InputExample], decomposer_model_path: str) -> List[str]:
+
+def text_to_qpl(examples: List[InputExample], decomposer_model_path: str, completer_model_path: str) -> List[str]:
+    # Decompose input questions
     decomposer_processor = QPLDecomposerProcessor()
-    composer_processor = QPLComposerProcessor()
-
     decomposer_model = AutoPeftModelForCausalLM.from_pretrained(decomposer_model_path).to("cuda")
     decomposer_tokenizer = AutoTokenizer.from_pretrained(decomposer_model_path)
-
     docomposer_trees = decompose(
         examples=examples,
         processor=decomposer_processor,
         model=decomposer_model,
         tokenizer=decomposer_tokenizer,
+        batch_size=16,
+        max_new_tokens=256,
     )
 
-    return docomposer_trees
+    # complete qpl for trees
+    completer_processor = QPLComposerProcessor()
+    completer_model = AutoPeftModelForCausalLM.from_pretrained(completer_model_path).to("cuda")
+    completer_tokenizer = AutoTokenizer.from_pretrained(completer_model_path)
+
+    complete(
+        trees=docomposer_trees,
+        processor=completer_processor,
+        model=completer_model,
+        tokenizer=completer_tokenizer,
+        batch_size=16,
+        max_new_tokens=256,
+    )
+
+    return [tree.qpl for tree in docomposer_trees]
 
 def decompose(
         examples: List[InputExample],
@@ -76,7 +102,7 @@ def decompose(
         return []
 
     # Create a QPL tree for each example
-    trees = [QPLTree(question=ex['question'], op=None) for ex in examples]
+    trees = [QPLTree(question=ex['question']) for ex in examples]
 
     # Use the decomposer model to generate the questions for the next layer of each QPL tree
     chat_templates = list(map(processor.to_chat_template, examples))
@@ -118,6 +144,18 @@ def decompose(
             tree.children = tuple(children)
     
     return trees
+
+
+def complete(
+        trees: List[QPLTree],
+        processor: QPLComposerProcessor,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizerBase,
+        batch_size: int = 16,
+        max_new_tokens: int = 512,
+    ) -> None:
+    # TODO implement this function
+    pass
 
 
 if __name__ == "__main__":
