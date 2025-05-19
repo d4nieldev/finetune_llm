3d15050e961d5b19eae661c938379d26063d5b7f
Daniel Ohayon <danielohayon2019@gmail.com>
2025-05-19 22:19:09 +0300

    moved creation of qpl tree from qpl lines to qpl utils

--- data/create_composer_data.py
+++ data/create_composer_data.py
@@ -5,6 +5,7 @@ from typing import Tuple, Union, List, Optional, Dict
 
 from tqdm import tqdm
 from datasets import load_dataset, Dataset, DatasetDict
+from utils.qpl.tree import QPLTree, get_qpl_tree
 
 
 class Operator(Enum):
@@ -39,21 +40,6 @@ class QDTree:
             "prefix_qpl": self.prefix_qpl,
             "qpl_line": self.qpl_line
         }
-    
-@dataclass
-class QPLTree:
-    qpl_row: Optional[str] = None
-    children: Optional[Union[Tuple["QPLTree"], Tuple["QPLTree", "QPLTree"]]] = None
-
-    @property
-    def op(self) -> Operator:
-        return Operator(self.qpl_row.split("=")[1].strip().split(" ")[0].strip())
-    
-    @property
-    def children_qpl(self) -> str:
-        if self.children is None:
-            return ""
-        return "\n".join([(child.children_qpl + "\n" + child.qpl_row).strip() for child in self.children]).replace("\n", " ; ")
 
 
 def get_decomposer_roots(decomposer_data: Dataset) -> List[QDTree]:
@@ -66,7 +52,7 @@ def get_decomposer_roots(decomposer_data: Dataset) -> List[QDTree]:
         if (question, db_id) not in q_to_tree:
             q_to_tree[(question, db_id)] = QDTree(question=question, db_id=db_id)
         return q_to_tree[(question, db_id)]
-    
+
     def construct_tree(row) -> QDTree:
         question = row["question"]
         db_id = row["db_id"]
@@ -74,7 +60,7 @@ def get_decomposer_roots(decomposer_data: Dataset) -> List[QDTree]:
 
         if tree is None:
             raise ValueError(f"Tree for question {question} not found in q_to_tree")
-        
+
         if row['op'] == 'Top':
             row['op'] = 'TopSort'
         tree.op = Operator(row["op"])
@@ -88,7 +74,7 @@ def get_decomposer_roots(decomposer_data: Dataset) -> List[QDTree]:
         tree.children = tuple([child for child in [child1, child2] if child is not None])
 
         return tree
-    
+
     all_trees = [construct_tree(row) for row in tqdm(decomposer_data, desc="Constructing QD trees")]
 
     return [tree for tree in all_trees if tree.parent is None]
@@ -104,40 +90,30 @@ def get_qpl_trees(nl2qpl_data: Dataset) -> Dict[Tuple[str, str], QPLTree]:
         db_id, qpl_code = [item.strip() for item in qpl.split("|")]
         qpl_rows = [qpl_row.strip() for qpl_row in qpl_code.split(";")]
 
-        row_nodes = [QPLTree() for _ in qpl_rows]
-        for qpl_row in qpl_rows:
-            line_numbers = [int(match) for match in re.findall(r"#(\d+)", qpl_row)]
-            row_id = line_numbers[0] - 1
-            children = [row_nodes[line_num - 1] for line_num in set(line_numbers[1:])]
-            assert len(children) <= 2, f"QPL row {qpl_row} has more than 2 children: {children}"
-            row_nodes[row_id].qpl_row = qpl_row
-            row_nodes[row_id].children = tuple(children)
-
-        q_to_qpl_tree[(question, db_id)] = row_nodes[-1]
+        q_to_qpl_tree[(question, db_id)] = get_qpl_tree(qpl_rows)
 
     return q_to_qpl_tree
 
 
 def complete_trees_qpl(
-        decomposer_roots: List[QDTree], 
-        q_to_qpl_tree: Dict[Tuple[str, str], QPLTree]
-    ) -> None:
+    decomposer_roots: List[QDTree],
+    q_to_qpl_tree: Dict[Tuple[str, str], QPLTree]
+) -> None:
 
     def complete_tree(root: QDTree, qpl_tree: QPLTree) -> None:
         assert len(root.children) <= len(qpl_tree.children), f"Number of children in QD tree ({len(root.children)}) should be less then or equal to number of children in QPL tree ({len(qpl_tree.children)})"
         assert root.op == qpl_tree.op, f"Operator in QD tree ({root.op.value}) does not match QPL tree ({qpl_tree.op.value})"
-        
+
         root.qpl_line = qpl_tree.qpl_row
         root.prefix_qpl = qpl_tree.children_qpl
 
         # change order when necessary
         if len(root.children) == 2 and root.children[0].op != qpl_tree.children[0].op:
             qpl_tree.children = qpl_tree.children[1], qpl_tree.children[0]
-        
+
         for root_child, qpl_child in zip(root.children, qpl_tree.children):
             complete_tree(root_child, qpl_child)
 
-    
     for root in tqdm(decomposer_roots, desc="Completing trees with QPL"):
         try:
             qpl_tree = q_to_qpl_tree[(root.question, root.db_id)]
@@ -152,6 +128,7 @@ def complete_trees_qpl(
             print(f"\t{e}")
             continue
 
+
 def create_composer_dataset(decomposer_roots: List[QDTree]) -> List[Dict]:
     def get_tree_rows(root: QDTree) -> List[Dict]:
         rows_to_return = []
@@ -162,22 +139,22 @@ def create_composer_dataset(decomposer_roots: List[QDTree]) -> List[Dict]:
             if root.parent is not None and root.parent.prefix_qpl is not None:
                 # enrich parent prefix QPL with child question
                 root.parent.prefix_qpl = root.parent.prefix_qpl.replace(
-                    root.qpl_line, 
+                    root.qpl_line,
                     f"{root.qpl_line}\t-- {root.question}"
                 )
             rows_to_return.append(
                 {
                     "db_id": root.db_id,
                     "parent_question": root.parent.question if root.parent else None,
-                    "question": root.question, 
+                    "question": root.question,
                     "prefix_qpl": root.prefix_qpl,
                     "op": root.op.value,
                     "qpl_line": root.qpl_line
                 }
             )
-        
+
         return rows_to_return
-    
+
     all_rows = [
         root_row
         for root in tqdm(decomposer_roots, desc="Creating dataset rows")
@@ -185,7 +162,7 @@ def create_composer_dataset(decomposer_roots: List[QDTree]) -> List[Dict]:
     ]
 
     return all_rows
-    
+
 
 if __name__ == "__main__":
     nl2qpl_dataset_id = "d4nieldev/nl2qpl-ds"
@@ -214,7 +191,7 @@ if __name__ == "__main__":
 
     # Upload the dataset to Hugging Face
     ds = DatasetDict({
-        split: Dataset.from_list(data) 
+        split: Dataset.from_list(data)
         for split, data in dataset.items()
     })
     ds.push_to_hub("d4nieldev/qpl-composer-ds")
