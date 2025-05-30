f940d2a03b09b0e278f01f2adc02440d55272327
Daniel Ohayon <danielohayon2019@gmail.com>
2025-05-30 10:03:24 +0300

    updated script

--- inference/qpl/types/test_llm.py
+++ inference/qpl/types/test_llm.py
@@ -1,20 +1,48 @@
+import sys
 import json
-import dspy
-from typing import List
+import random
+from typing import List, Dict, Tuple
 
 from inference.qpl.types.schema_types import DBSchema
+
+import dspy
+
+SEED = 42
+LM_ID = "openai/gpt-4o-mini"
+SAMPLE_SIZE = 10
+FEWSHOT_TRAIN_RATIO = 0.5
+FEWSHOT_EXAMPLES = 2
+FILENAME = f"predicted_types_concert_singer_seed{SEED}_sample{SAMPLE_SIZE}_train_ratio{FEWSHOT_TRAIN_RATIO}_fewshot{FEWSHOT_EXAMPLES}"
+
+
+random.seed(SEED)
+
+def split_train_test(dataset: List[Dict], train_ratio: float = 0.8) -> Tuple[List[dspy.Example], List[dspy.Example]]:
+    """Split dataset into train and test sets."""
+    split_index = int(len(dataset) * train_ratio)
+    inputs_dataset = [
+        dspy.Example(
+            database_schema=str(db_schemas[example["db_id"]]),
+            entities=[str(entity) for entity in db_schemas[example["db_id"]].entities],
+            question=example["question"],
+            predicted_type=example["type"]
+        ).with_inputs("database_schema", "entities", "question")
+        for example in dataset
+    ]
+    random.shuffle(dataset)
+    return inputs_dataset[:split_index], inputs_dataset[split_index:]
 
 
 class TypeClassification(dspy.Signature):
     """Predict the type of the result that would be returned by executing an SQL query that correctly answers the question.
 
 The possible types are:
-    - {entity} - The result is a row that contains multiple columns of an entity entity including {entity}'s primary key. {entity} should be replaced with one of the entities in the schema.
-    - Partial[{entity}] - The result is a row that contains multiple columns of an entity entity but does not include {entity}'s primary key. {entity} should be replaced with one of the entities in the schema.
+    - {entity} - The result is a row that contains {entity}'s primary key (optionally with other columns of {entity}). {entity} should be replaced with one of the entities in the schema.
+    - Partial[{entity}] - The result is a row that contains multiple columns of an entity entity but DOES NOT INCLUDE {entity}'s primary key. {entity} should be replaced with one of the entities in the schema.
     - Reduced[{entity}] - The result is the outcome of a computation on a List[{entity}] or List[Partial[{entity}]]. {entity} should be replaced with one of the entities in the schema.
     - Number - The result is just a number that is not the result of a calculation on a List of entities.
-    - Union[{type_1}, {type_2}, ..., {type_n}] - The result is a row that is a combination of a subset of the possible types defined above..
-    - List[{type}] - if the result returns a list of any of the types defined above. {type} should be replaced with one of the possible types.
+    - Union[{type_1}, {type_2}, ..., {type_n}] - The result is a row that is a combination of a subset of the possible types defined above.
+    - List[{type}] - if the result returns multiple rows, where each row is of type {type}. {type} should be replaced with one of the possible types defined above.
 """
     database_schema: str = dspy.InputField(desc="Database schema described in DDL.")
     entities: List[str] = dspy.InputField(desc="Entities in the schema.")
@@ -22,28 +50,59 @@ The possible types are:
     predicted_type: str = dspy.OutputField(desc="Predicted type.")
 
 
+class TypeClassificationModule(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.generator = dspy.ChainOfThought(TypeClassification)
+    
+    def forward(self, database_schema: str, entities: List[str], question: str) -> TypeClassification:
+        """Predict the type of the result that would be returned by executing an SQL query that correctly answers the question."""
+        return self.generator(database_schema=database_schema, entities=entities, question=question)
+
+
 # Configure LLM
-lm = dspy.LM('openai/gpt-4o-mini')
+lm = dspy.LM(LM_ID)
 dspy.configure(lm=lm)
 
 # Load database schema from JSON file
-with open("data/qpl/spider/db_schemas.json", "r") as f:
-    db_schemas_json = json.load(f)
-db_schemas = DBSchema.from_db_schemas(db_schemas_json)
+db_schemas = DBSchema.from_db_schemas_file("data/qpl/spider/db_schemas.json")
 
-# Configure prompt
-pred = dspy.Predict(TypeClassification)
-schema = db_schemas["concert_singer"]
-database_schema = str(schema)
-entities = ", ".join([str(entity) for entity in schema.entities])
-question = "Show name, country, age for all singers ordered by age from the oldest to the youngest."
+# Load and prepare the dataset
+with open("data/qpl/types/question_types_concert_singer.json", "r") as f:
+    dataset = json.load(f)
+    if SAMPLE_SIZE > 0:
+        dataset = random.sample(dataset, k=SAMPLE_SIZE)
 
-# Predict type
-predicted_type = pred(
-    database_schema=database_schema,
-    entities=entities,
-    question=question
-).predicted_type
+train, test = split_train_test(dataset, train_ratio=FEWSHOT_TRAIN_RATIO)
 
-# Show prompt
-print(lm.inspect_history(1))
+generate_type = TypeClassificationModule()
+fewshot = dspy.LabeledFewShot(k=FEWSHOT_EXAMPLES)
+compiled_fewshot = fewshot.compile(
+    generate_type,
+    trainset=train,
+)
+
+# Predict types
+predicted_types = compiled_fewshot.batch(test)
+
+# Save logs
+sys.stdout = open(f"data/qpl/types/{FILENAME}.txt", "w")
+lm.inspect_history(len(predicted_types))
+sys.stdout.close()
+
+# Save results
+with open(f"data/qpl/types/{FILENAME}.json", "w") as f:
+    json.dump(
+        [
+            {
+                "db_id": example["db_id"],
+                "question": example["question"],
+                "actual_type": example["type"],
+                "predicted_type": pt.predicted_type,
+                "reasoning": pt.reasoning if hasattr(pt, 'reasoning') else None
+            }
+            for example, pt in zip(dataset, predicted_types)
+        ],
+        f, 
+        indent=2
+    )
