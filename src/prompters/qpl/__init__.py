e90d3bf492c99ad08de961f47b44ff3ba1bfd8f9
Daniel Ohayon <danielohayon2019@gmail.com>
2025-08-02 00:14:53 +0300

    imported prompters for registry

--- src/prompters/qpl/__init__.py
+++ src/prompters/qpl/__init__.py
@@ -1,3 +1,5 @@
 from .decomposer import QPLDecomposerPrompter
 from .completer import QPLCompleterPrompter
 from .decomposer_cot import QPLDecomposerCotPrompter
+from .completer_cot import QPLCompleterCotPrompter
+from .merged_cot import QPLMergedCotPrompter
