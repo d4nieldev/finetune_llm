6975fffe2f57ac3e7cdda33c7638b2d29acf92c8
Daniel Ohayon <danielohayon2019@gmail.com>
2025-08-01 19:54:56 +0300

    generic path for metadata

--- src/utils/qpl/paths.py
+++ src/utils/qpl/paths.py
@@ -4,6 +4,7 @@ from pathlib import Path
 SPIDER_INPUT_DIR = Path("input/qpl/spider")
 DB_SCHEMAS_JSON_PATH = SPIDER_INPUT_DIR / "db_schemas.json"
 DB_CONTENT_PATH = SPIDER_INPUT_DIR / "db_content.json"
+DB_PROFILES_PATH = SPIDER_INPUT_DIR / "db_metadata.json"
 
 SPIDER_DB_CREATION_DIR = SPIDER_INPUT_DIR / "db_creation"
 DB_CREATION_PICKLE_DATA_PATH = SPIDER_DB_CREATION_DIR / "data_to_insert_no_alters.pkl"
